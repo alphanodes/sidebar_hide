73db84f8459ce0d929dee095a8d18286fc904663
Alexander Meindl <alex@alphanodes.com>
2019-02-25 16:56:43 +0100

    Do not load sidebar hide on agile charts

--- init.rb
+++ init.rb
@@ -1,11 +1,14 @@
-# encoding: utf-8
 require_dependency 'sidebar_hook_listener'
 
 Redmine::Plugin.register :sidebar_hide do
   name 'Sidebar Hide Plugin'
   author 'Berk Demirkır'
   description 'This plugin provides ability to hide sidebar'
-  version '0.0.8'
-  url 'https://github.com/bdemirkir/sidebar_hide'
-  author_url 'https://github.com/bdemirkir'
+  version '0.0.8-fix'
+
+  begin
+    requires_redmine_plugin :additionals, version_or_higher: '2.0.19'
+  rescue Redmine::PluginNotFound
+    raise 'Please install additionals plugin (https://github.com/alphanodes/additionals)'
+  end
 end
--- lib/sidebar_hook_listener.rb
+++ lib/sidebar_hook_listener.rb
@@ -1,8 +1,14 @@
 class SidebarHookListener < Redmine::Hook::ViewListener
-	render_on :view_layouts_base_content, :partial => 'sidebar/hideButton_partial'
+  render_on :view_layouts_base_content, partial: 'sidebar/hide_button'
 
-  def view_layouts_base_html_head(_context = {})
-    stylesheet_link_tag('sidebar_hide', :plugin => 'sidebar_hide', :media => 'all') +
-      javascript_include_tag('sidebar_hide', :plugin => 'sidebar_hide')
+  def view_layouts_base_html_head(context = {})
+    if Redmine::Plugin.installed?('redmine_agile')
+      info = Rails.application.routes.recognize_path(context[:request].url)
+      return if info.present? && (info[:controller] == 'agile_charts' && info[:action] == 'show' ||
+                                info[:controller] == 'versions' && info[:action] == 'show')
+    end
+
+    stylesheet_link_tag('sidebar_hide', plugin: 'sidebar_hide', media: 'all') +
+      javascript_include_tag('sidebar_hide', plugin: 'sidebar_hide')
   end
 end
