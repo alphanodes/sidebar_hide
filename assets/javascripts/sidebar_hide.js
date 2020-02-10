3667020afe7486a1a8fa1899910dea3cee9f869b
Alexander Meindl <alex@alphanodes.com>
2020-02-10 11:47:39 +0100

    Support popup mode

--- assets/javascripts/sidebar_hide.js
+++ assets/javascripts/sidebar_hide.js
@@ -5,32 +5,18 @@ function setCookie(c_name, value, exdays) {
   document.cookie = c_name + '=' + c_value + ';path=/';
 }
 
-function getCookie(c_name) {
-  var i;
-  var x;
-  var y;
-  var ARRcookies = document.cookie.split(';');
-  for (i = 0; i < ARRcookies.length; i++) {
-    x = ARRcookies[i].substr(0, ARRcookies[i].indexOf('='));
-    y = ARRcookies[i].substr(ARRcookies[i].indexOf('=') + 1);
-    x = x.replace(/^\s+|\s+$/g, '');
-    if (x == c_name)
-      return unescape(y);
-  }
-}
-
-function hideSideBar()
-{
+/* exported hideSideBar */
+function hideSideBar(popup) {
   if ($('#sidebar').is(':visible')) {
     $('#sidebar').addClass('sidebar_hidden');
     $('#content').addClass('sidebar_hidden');
     $('#hideSidebarButton').addClass('sidebar_hidden');
-    setCookie('sidebar_hide', 'hide', 100);
+    if (!popup) { setCookie('sidebar_hide', 'hide', 100); }
   } else {
     $('#sidebar').removeClass('sidebar_hidden');
     $('#content').removeClass('sidebar_hidden');
     $('#hideSidebarButton').removeClass('sidebar_hidden');
-    setCookie('sidebar_hide', 'show', 100);
+    if (!popup) { setCookie('sidebar_hide', 'show', 100); }
   }
 }
 
